6366ea69c0692acf83260f2541d67d3d03eb774c
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-06-30 07:11:10 +0000

    Added functions to append to list route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+
+
 $(() => {
   $.ajax({
     method: "GET",
@@ -8,3 +10,39 @@ $(() => {
     }
   });;
 });
+
+LoadList();
+console.log("got here")
+function LoadList(){
+  $.ajax({
+    method:"GET",
+    url: "/mylist/items",
+    dataType: "json"
+  }).done((items) => {
+    items.forEach( function(element){
+
+      var createdEl = createElement(element);
+      console.log("Got Here Buddy")
+      console.log(createdEl)
+
+      $('.thisDude').append(createdEl);
+
+    })
+  })
+}
+
+function createElement(obj){
+  var $article = $('<article>');
+  var category = obj.category;
+  var content = obj.content;
+
+  var $category = $(`<p>${category}</p>`);
+  var $content = $(`<p>${content}</p>`);
+
+  $($article).append($category);
+  $($article).append($content);
+
+  return $article
+
+}
+
